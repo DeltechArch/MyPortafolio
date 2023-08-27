36afae124abfeaf0dc23e82a7ae4eb44c4565c37
Acro <deltecharch@gmail.com>
2023-08-26 23:53:01 -0600

    ajustes nuevos

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@
 function mostrarPerfil() {
 
     const sectionPerfil = document.getElementById("Perfil");
-    const imagenPerfil = document.createElement("img");
-    imagenPerfil.setAttribute("src", "./assets/imagens/yonew2.avif");
-    imagenPerfil.setAttribute("alt", "imagen de perfil");
 
     const h1Perfil = document.createElement("h1");
     h1Perfil.innerHTML = "JUAN DELGADILLO <br> Software Developer.";
@@ -13,7 +10,6 @@ function mostrarPerfil() {
     const pPerfil = document.createElement("p");
     pPerfil.innerHTML = "Apasionado por la tecnologia y programacion. <br> En busqueda de retos.";
 
-    sectionPerfil.appendChild(imagenPerfil);
     sectionPerfil.appendChild(h1Perfil);
     sectionPerfil.appendChild(pPerfil);
 }
@@ -24,15 +20,26 @@ function mostrarEducacion(){
     const sectionEducacion = document.getElementById("Educacion");
 
     h1Educacion = document.createElement("h1");
-    h1Educacion.innerHTML = "Educacion en Desarrollo Web";
+    h1Educacion.innerHTML = "Educacion:";
     divEducacion = document.createElement("div");
-    h2Educacion =  document.createElement("h2");
-    h2Educacion.innerHTML = "Platzi";
-    pEducacion =  document.createElement("p");
-    pEducacion.innerHTML ="27/Diciembre/2022";
 
-    divEducacion.appendChild(h2Educacion);
-    divEducacion.appendChild(pEducacion);
+    //primer educacion    
+    h2Platzi =  document.createElement("h2");
+    h2Platzi.innerHTML = "Platzi";
+    pPlatzi =  document.createElement("p");
+    pPlatzi.innerHTML ="27/Diciembre/2022";
+
+    // otra educacion
+    h2Udemy = document.createElement("h2");
+    h2Udemy.innerHTML = "Udemy";
+    pUdemy =  document.createElement("p");
+    pUdemy.innerHTML ="21/agosto/2023";
+
+    divEducacion.appendChild(h2Platzi);
+    divEducacion.appendChild(pPlatzi);
+    divEducacion.appendChild(h2Udemy);
+    divEducacion.appendChild(pUdemy);
+    
     sectionEducacion.appendChild(h1Educacion);
     sectionEducacion.appendChild(divEducacion);
 }
